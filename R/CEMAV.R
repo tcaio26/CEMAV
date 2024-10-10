dd571645e766cc1fff1d86f31dea26cecb4e1ae2
tcaio26 <caiotheodore26@gmail.com>
2024-10-10 09:58:52 -0300

    Inicializando o reporsitório

---
+++ R/CEMAV.R
@@ -0,0 +1,9 @@
+#' @name CEMAV
+#'
+#' @docType package
+#'
+#' @description
+#' Pacote destinado às funções criadas ao longo da iniciação científica "Introdução às cadeias estocásticas de memória variável"
+#'
+#' @export
+"_PACKAGE"
